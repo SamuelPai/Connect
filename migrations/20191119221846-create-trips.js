9400a3b8a74cef09bd9c04c44c1a82e932259035
Max Reinmueller <maxreinmueller@gmail.com>
2019-11-19 14:22:42 -0800

    fixed trip table

---
+++ migrations/20191119221846-create-trips.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Trips', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      Description: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Trips');
+  }
+};
