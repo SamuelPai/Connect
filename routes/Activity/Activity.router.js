f2ed701b2266d9e1cfc042d3080150f27298be19
Max Reinmueller <maxreinmueller@gmail.com>
2019-11-19 15:27:49 -0800

    all routes working

--- routes/Activity/Activity.router.js
+++ routes/Activity/Activity.router.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/Activity/Activity.controller');
-
 // MIDDLEWARE - Router Specific
 router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
+ console.log('Time: ', Date.now());
+ next();
 });
-
 // ROUTES
-router.post('/Activity', controller.create);
-router.get('/Activity', controller.getAll);
-router.get('/Activity/:id', controller.getById);
-router.put('/Activity/:id', controller.updateById);
-router.delete('/Activity/:id', controller.updateById);
-
+// router.post('/Activity', controller.create);
+router.get('/', controller.getAll);
+// router.get('/Activity/:id', controller.getById);
+// router.put('/Activity/:id', controller.updateById);
+// router.delete('/Activity/:id', controller.updateById);
 module.exports = router;
