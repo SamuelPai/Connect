ec6cdf11e1653d3bbbb1e9aaf7c4a580c6e7fd22
Samuel Pai <samuelpai16@gmail.com>
2019-11-20 20:34:15 -0800

    added API endpoint to pull trip data

--- client/src/pages/LandingPage/LandingPage.component.js
+++ client/src/pages/LandingPage/LandingPage.component.js
@@ -3,16 +3,42 @@ import { LandingPageProps } from './LandingPage.propTypes'
 import { ReactRouterDetails } from '../../components/ReactRouterDetails/ReactRouterDetails.component';
 import styles from './LandingPage.styles.scss';
 import LoginButton from "../../components/LoginButton/LoginButton";
+import tripAPI from "../../utils/tripAPI";
 
 
 export class LandingPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      trips: {
+        title: "",
+        location:"",
+        description: "",
+        date: "",
+        image: ""
+        
+      }
+    };
   }
 
   componentDidMount() {
     console.log('componentDidMount()', this.props);
+    tripAPI.getTrips().then(res => {
+      console.log("RESPONSE.DATA.TRIPS.TITLE", res.data.trips[0].title);
+      var data = res.data.trips[0];
+      this.setState({
+        trips: {
+          title: data.title,
+          location: data.location,
+          description: data.description,
+          date: data.date,
+          image: data.image
+        }
+      });
+    })
+    .catch(err => {
+      console.log("ERROR, could not load trips", err);
+    })
   }
 
   componentDidUpdate(prevProps, prevState, prevSnapshot) {
@@ -24,6 +50,11 @@ export class LandingPage extends React.Component {
     return (
       <div>
         <LoginButton />
+        <h1>{this.state.trips.title}</h1>
+        <h2>Location: {this.state.trips.location}</h2>
+        <p>Description: {this.state.trips.description}</p>
+        <p>Date: {this.state.trips.date}</p>
+        <img src={this.state.trips.image} />
       </div>
     );
   }
