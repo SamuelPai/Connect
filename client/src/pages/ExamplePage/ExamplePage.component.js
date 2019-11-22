66983c6c4aa11e8e43264bb9c50fd5b71c769934
Max Reinmueller <maxreinmueller@gmail.com>
2019-11-22 14:14:15 -0800

    example page is making api call and mapping the data

--- client/src/pages/ExamplePage/ExamplePage.component.js
+++ client/src/pages/ExamplePage/ExamplePage.component.js
@@ -1,25 +1,39 @@
 import React from "react";
 import Example from "../../components/Example/Example.component";
-import friends from '../../utils/friends.json'
+import friends from '../../utils/friends.json';
+import tripAPI from '../../utils/tripAPI';
 
 export class ExamplePage extends React.Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    trips: []
+  };
+
+  componentDidMount() {
+    this.getTrips();
+  }
+
+  getTrips = () => {
+    tripAPI.getTrips()
+      .then(res => {
+        var tripArray = res.data.trips
+        console.log(tripArray);
+        this.setState({ trips: tripArray })
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
     return (
       <div>
-        {this.state.friends.map(friend => (
+        {this.state.trips.map(trip => (
           <Example
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            title={friend.title}
-            image={friend.image}
-            Description={friend.Description}
-            location={friend.location}
+            id={trip.id}
+            key={trip.id}
+            title={trip.title}
+            image={trip.image}
+            Description={trip.Description}
+            location={trip.location}
           />
         ))}
       </div>
