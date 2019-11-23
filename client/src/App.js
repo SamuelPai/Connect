3ea8622c5fbc7ee8f95680e236faf947f9e4344d
kaur1081 <karishmawalia@gmail.com>
2019-11-23 12:25:19 -0800

    issue not resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -10,48 +9,26 @@ import { NoMatch } from './components/NoMatch';
 import { LandingPage } from './pages/LandingPage/LandingPage.component';
 import { TripPage } from './pages/TripPage/TripPage.component';
 
-=======
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from "./components/Home/Home.component";
-import { About } from "./components/About/About.component";
-import { CheeseDetail } from "./pages/CheeseDetail/CheeseDetail.component";
-import { ExamplePage } from "./pages/ExamplePage/ExamplePage.component";
-import { NoMatch } from "./components/NoMatch";
-import { LandingPage } from "./pages/LandingPage/LandingPage.component";
->>>>>>> 87016c1ad26b9f08350b2f5fb25bd1c3e3973c22
 import Profile from "./components/Profile/Profile";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import { MyTripPagePage } from "./pages/MyTripPagePage/MyTripPagePage.component";
-import MyTrip from "./components/MyTrip/MyTrip";
-import NavBar from "./pages/NavBar/Navbar";
-import FooterPage from "./pages/FooterPage/Footer";
+
 function App() {
+  
+  
 
   return (
     <Router>
-<<<<<<< HEAD
       
-=======
-      <NavBar />
->>>>>>> 87016c1ad26b9f08350b2f5fb25bd1c3e3973c22
       <Switch>
-        <Route exact path="/" component={LandingPage} /> */}
+        <Route exact path="/" component={LandingPage} />
         <PrivateRoute path="/profile" component={Profile} />
         <Route exact path="/about" component={About} />
         <Route exact path="/cheese/:id" component={CheeseDetail} />
-        <Route exact path="/example" component={ExamplePage} />
+        <Route exact path="/example/:id" component={ExamplePage} />
         <Route exact path="/landing" component={LandingPage} />
-<<<<<<< HEAD
         <Route exact path="/trip" component={TripPage} />
-=======
-        <Route exact path="/MyTrip" component={MyTrip} />
-        <Route exact path="/NavBar" component={NavBar} />
->>>>>>> 87016c1ad26b9f08350b2f5fb25bd1c3e3973c22
         <Route component={NoMatch} />
       </Switch>
-      <FooterPage />
     </Router>
   );
 }
