951301501d5486aec663b27730a4d46e1619053a
Divya <enjamuridivya@gmail.com>
2019-11-26 21:46:37 -0800

    heroku deploying

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,60 +1,75 @@
-import React from 'react';
-import { Media } from 'reactstrap';
+import React from "react";
+import { Media } from "reactstrap";
 // import "./s"
-import { InputGroup, InputGroupText, InputGroupAddon, Input } from 'reactstrap';
+import { InputGroup, InputGroupText, InputGroupAddon, Input } from "reactstrap";
 
 const Search = () => {
   return (
     <div>
-<br />
-<InputGroup>
+      <br />
+      <InputGroup>
         <Input />
-        <InputGroupAddon className="searchBar"addonType="append">
+        <InputGroupAddon className="searchBar" addonType="append">
           <InputGroupText>Search</InputGroupText>
         </InputGroupAddon>
       </InputGroup>
- <br />   
-    <Media>
+      <br />
+      <Media>
         <Media body>
-        <Media heading>
-          Media heading
-        </Media>
-        <Media>
+          <Media heading>Media heading</Media>
+          <Media>
             <Media left href="#">
-              <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+              <Media
+                object
+                data-src="holder.js/64x64"
+                alt="Generic placeholder image"
+              />
             </Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </Media>
+        </Media>
       </Media>
-    </Media>
-    </Media>
-    <br />   
-    <Media>
+      <br />
+      <Media>
         <Media body>
-        <Media heading>
-          Media heading
-        </Media>
-        <Media>
+          <Media heading>Media heading</Media>
+          <Media>
             <Media left href="#">
-              <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+              <Media
+                object
+                data-src="holder.js/64x64"
+                alt="Generic placeholder image"
+              />
             </Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </Media>
+        </Media>
       </Media>
-    </Media>
-    </Media>
-    <br />   
-    <Media>
+      <br />
+      <Media>
         <Media body>
-        <Media heading>
-          Media heading
-        </Media>
-        <Media>
+          <Media heading>Media heading</Media>
+          <Media>
             <Media left href="#">
-              <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+              <Media
+                object
+                data-src="holder.js/64x64"
+                alt="Generic placeholder image"
+              />
             </Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </Media>
+        </Media>
       </Media>
-    </Media>
-    </Media>
     </div>
   );
 };
