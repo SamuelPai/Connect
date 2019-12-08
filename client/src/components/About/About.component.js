fab81a2ce5c4116c5d468c70f18472a874f5100f
Samuel Pai <samuelpai16@gmail.com>
2019-12-07 20:16:43 -0800

    updates

--- client/src/components/About/About.component.js
+++ client/src/components/About/About.component.js
@@ -12,7 +12,7 @@ export const About = (props) => {
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
         </CardBody>
-        <img width="100%" src="/assets/318x180.svg"/>
+        <img width="100%" src="/assets/318x180.svg" alt="for about the trip"/>
         <CardBody>
           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
           <CardLink href="#">Card Link</CardLink>
